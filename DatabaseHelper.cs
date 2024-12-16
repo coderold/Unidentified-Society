40495551e32df2e2cf26016138c715881ce66b28
LanceMontierro <lancemontierro21@gmail.com>
2024-12-16 15:35:32 +0800

    KOPAL KOPAL

--- DatabaseHelper.cs
+++ DatabaseHelper.cs
@@ -97,6 +97,7 @@ namespace UnidentifiedSociety
                     .Child(name)
                     .DeleteAsync();
 
+             
             }
             catch (Exception ex)
             {
